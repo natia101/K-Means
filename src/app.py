88ab02a8bb2046f5ca12fadf7348fac7ba49ed66
natia101 <natia09@hotmail.com>
2022-10-01 22:57:05 +0000

    final

--- src/app.py
+++ src/app.py
@@ -1,4 +1,31 @@
-from utils import db_connect
-engine = db_connect()
 
-# your code here
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import sklearn
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+import pickle
+
+# Load the dataset
+
+url= 'https://raw.githubusercontent.com/4GeeksAcademy/k-means-project-tutorial/main/housing.csv'
+df_raw = pd.read_csv(url)
+
+X_raw=df_raw[['Latitude','Longitude','MedInc']]
+
+# Scalation
+sc = StandardScaler()
+X = sc.fit_transform(X_raw)
+
+# Clusterization
+clf=KMeans(n_clusters=6,random_state=408) # Define model
+cluster=clf.fit(X)# fit model
+
+# Save the model as a pickle
+filename = '/workspace/K-Means/models/final_model.pkl'
+pickle.dump(clf, open(filename,'wb'))
